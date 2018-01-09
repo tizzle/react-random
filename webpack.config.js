455ab77d63c194d000c086baf6fc58f3651ae4f3
Till Hinrichs <till@eightdaysaweek.cc>
2018-01-10 00:19:26 +0100

    added webpack bundler for UMD package

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/umd'),
+    filename: 'react-random.min.js',
+    libraryTarget: 'umd',
+    library: 'react-random'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
